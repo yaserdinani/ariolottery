654f9ee292bf47421a843c7e1bd8a9de6976118a
yaserdinani <yaserdinani1999@gmail.com>
2022-06-05 00:16:23 +0430

    + :)

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,4 @@ Route::get('/login',App\Http\Livewire\Auth\Login::class)->name('login');
 
 Route::group(["middleware"=>"auth"],function(){
     Route::get('/users',App\Http\Livewire\User\Index::class)->name('users.index');
-    // Route::get('/user/create',App\Http\Livewire\User\Create::class)->name('users.create');
-    // Route::get('/user/update/{id}',App\Http\Livewire\User\Update::class)->name('users.update');
-    // Route::get('/user/{id}',App\Http\Livewire\User\Show::class)->name('users.show');
 });
