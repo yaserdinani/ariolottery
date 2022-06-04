6f75b6d08e25135f9482db0a013524b433d2c924
yaserdinani <yaserdinani1999@gmail.com>
2022-06-04 21:13:10 +0430

    first commit

---
+++ app/Http/Livewire/Auth/Login.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire\Auth;
+
+use Livewire\Component;
+
+class Login extends Component
+{
+    public function render()
+    {
+        return view('livewire.auth.login')
+        ->layout('layouts.auth');
+    }
+}
---
+++ app/Http/Livewire/User/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.index')
+        ->layout('layouts.panel');
+    }
+}
