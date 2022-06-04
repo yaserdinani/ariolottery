d0c6e31099893cad8fdba87766235acf4efdd2b0
yaserdinani <yaserdinani1999@gmail.com>
2022-06-04 23:30:27 +0430

    + add header component + refactor panel layout file

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -4,11 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     @livewireStyles
-    <title>سامانه قرعه کشی آریو ایده</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>@yield('title')-آریو ایده</title>
 </head>
 <body>
-    
+    <livewire:component.header />
+    <div class="container" dir="rtl">
+        {{$slot}}
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
     @livewireScripts
 </body>
 </html>
---
+++ resources/views/livewire/component/header.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="text-align:left;">
+    <a class="navbar-brand" href="#">آریو ایده گستر سپاهان</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{route('users.index')}}">کاربران <span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">خروج</button>
+      </form>
+    </div>
+</nav>
