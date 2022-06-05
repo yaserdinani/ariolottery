304038d148d8b83c6d05c504fb0ee71932542781
yaserdinani <yaserdinani1999@gmail.com>
2022-06-05 12:50:50 +0430

    + add Setting model + add Setting migration file + add Setting seeder + refactor app layout file

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('settings')->insert([
+            "tmp_level"=>0,
+            "max_level"=>0
+        ]);
+    }
+}
